f72ea65fe41e3d2d707e58504f7b0f44ea7030c9
fayyazarshad <fayyazarshad@folio3.com>
2020-02-05 22:24:46 -0800

    first commit

---
+++ AsyncStorageUserProfileService/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import LocalStorage from './local_storage';
+
+const App: () => React$Node = () => {
+  return <LocalStorage />;
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+# user-profile-service-samples
